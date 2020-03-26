e10337a0f2e707c125a6ac25429a194f4e4b1d93
Renan Cunha <rfvcunha@hotmail.com>
2020-03-26 15:03:53 -0300

    Finalizando Pagina de Casos

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {SafeAreaView, View, Image, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Feather from 'react-native-vector-icons/Feather';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 // import { Container } from './styles';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -19,6 +34,32 @@ export default function Incidents() {
       <Text style={styles.description}>
         Escolha um dos casos abaixo e salve o dia!
       </Text>
+
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        style={styles.incidentsList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APIPA</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Cachorro atropelado</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}>
+              <Text style={styles.detailsButtonText}>Ver Mais Detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
